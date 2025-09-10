b4ea3eea32cd04487f6b1ab92e8526dc5d178c54
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2025-09-10 14:53:38 +0200

    fix-ecs: Removed unused feature

--- ecs/src/lib.rs
+++ ecs/src/lib.rs
@@ -1,12 +1,10 @@
 #![feature(impl_trait_in_assoc_type)]
-#![feature(type_alias_impl_trait)]
 #![feature(associated_type_defaults)]
 #![feature(try_blocks)]
 #![feature(macro_metavar_expr)]
 #![feature(assert_matches)]
 #![feature(debug_closure_helpers)]
 #![feature(never_type)]
-#![feature(option_zip)]
 
 #![forbid(unsafe_code)]
 
